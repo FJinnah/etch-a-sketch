8af9fa6182715b52591d1aded0224bc62ab73c5e
Farzeen Jinnah <farzeenj@gmail.com>
2022-09-01 20:08:20 -0600

    Add a button to delete all generated divs
    Clear grid button now deletes all divs inside the container. This fixes
    the issue I was having with the new grid appending to the bottom of the
    old grid.

--- script.js
+++ script.js
@@ -1,19 +1,75 @@
+const gridContainer = document.querySelector('#grid-container');
 //Function to create 256 divs
 function makeGrid() {
-  const gridContainer = document.querySelector('#grid-container');
 
   for (let i = 0; i < 256; i++) {
     let gridSquare = document.createElement('div');
     gridSquare.classList.add('drawSquare');
-    gridSquare.innerText = i;
+    // gridSquare.innerText = i;
+    gridContainer.appendChild(gridSquare);
+    //This part changes the background colour of each div to black when the user hovers over it
     gridSquare.addEventListener('mouseover', () => {
       gridSquare.style.backgroundColor = 'black';
     })
 
-    gridContainer.appendChild(gridSquare);
+  }
+}
+makeGrid();
+
+//Clear the grid
+const clearGrid = document.querySelector('#clearButton');
+clearGrid.addEventListener('click', () => {
+  const selectGrid = document.querySelectorAll('.drawSquare');
+  selectGrid.forEach(function() {
+    let removal = document.querySelector('.drawSquare');
+    gridContainer.removeChild(removal);
+  })
+})
+
+//Set new grid
+const newGrid = document.querySelector('#gridButton');
+newGrid.addEventListener('click', buttonPrompt);
+function buttonPrompt() {
+  const userInput = prompt('Please select a grid size between 1 and 100.');
+  if (userInput >= 1 && userInput <= 100) {
+    gridContainer.style.gridTemplateColumns = 'repeat(userInput, auto)';
+    gridContainer.style.gridTemplateRows = 'repeat(userInput,auto)';
+    for (let i = 0; i < userInput; i++) {
+      let gridSquare = document.createElement('div');
+      gridSquare.classList.add('drawSquare');
+      // gridSquare.innerText = i;
+      gridContainer.appendChild(gridSquare);
+      //This part changes the background colour of each div to black when the user hovers over it
+      gridSquare.addEventListener('mouseover', () => {
+        gridSquare.style.backgroundColor = 'black';
+      })  
+    }
+  } else {
+    const userInput = prompt('Please select a grid size between 1 and 100.');
+
   }
 }
 
-makeGrid();
 
+// const button = document.querySelector('#gridButton');
+// button.addEventListener('click', buttonPrompt);
+// function buttonPrompt() {
+//   const userInput = prompt('Please select a grid size (maximum of 100)');
+//   const gridSize = 500 / parseInt(userInput);
+//   // return gridSize;  
+//   const gridContainer = document.querySelector('#grid-container');
+
+//   for (let i = 0; i < gridSize; i++) {
+//     let gridSquare = document.createElement('div');
+//     gridSquare.classList.add('drawSquare');
+//     // gridSquare.innerText = i;
+//     //This part changes the background colour of each div to black when the user hovers over it
+//     gridContainer.appendChild(gridSquare);
+    
+//     gridSquare.addEventListener('mouseover', () => {
+//       gridSquare.style.backgroundColor = 'black';
+//     })
+
+//   }
+// }
 
