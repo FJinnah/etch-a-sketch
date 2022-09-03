5e44f929c42fd44ad89651204ad332dfed676f88
Farzeen Jinnah <farzeenj@gmail.com>
2022-09-02 22:15:27 -0600

    Add a rainbow mode for random colour selection

--- script.js
+++ script.js
@@ -55,3 +55,31 @@ function buttonPrompt() {
   }
 }
 
+const newRainbow = document.querySelector('#rainbowButton');
+newRainbow.addEventListener('click', rainbowPrompt);
+function rainbowPrompt() {
+  const selectGrid = document.querySelectorAll('.drawSquare');
+  selectGrid.forEach(function() {
+    let removal = document.querySelector('.drawSquare');
+    gridContainer.removeChild(removal);
+  })
+  const userInput = prompt('Please select a grid size between 1 and 100.');
+  const finalNumber = parseInt(userInput);
+  if (finalNumber >= 1 && finalNumber <= 100) {
+    gridContainer.style.gridTemplateColumns = `repeat(${finalNumber}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${finalNumber}, 1fr)`;
+    for (let i = 0; i < (finalNumber ** 2); i++) {
+      let gridSquare = document.createElement('div');
+      gridSquare.classList.add('drawSquare');
+      gridContainer.appendChild(gridSquare);
+      //This part changes the background colour of each div to black when the user hovers over it
+        gridSquare.addEventListener('mouseover', () => {
+          let randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+          gridSquare.style.backgroundColor = randomColor;
+      })  
+    }
+  } else {
+    buttonPrompt();
+
+  }
+}
